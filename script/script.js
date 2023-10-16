45f7cfb2b679b053dec7e585eb9e02a655968196
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-10-16 16:29:01 +0200

    added based algorithm

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
 const output = document.getElementById('risultato');
+const lista = [];
+let elemento = '';
+
+while(elemento !== 'stop' && elemento !== 'Stop' && elemento !== 'STOP'){
+    elemento = prompt('Inserire prodotto, o digitare "stop" per concludere la lista ');
+    lista.push(elemento);
+}
+lista.pop();
+
+let msg = '';
+for(let j = 0; j < lista.length; j++){
+    msg += `
+    <div>
+        ${lista[j]}
+    </div>
+    `;
+}
+
+output.innerHTML = msg;
